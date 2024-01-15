d7e18662d97deb4f5e37189ab4d81d4eb8930b68
Oleg <oleg.kov11@gmail.com>
2024-01-15 15:07:06 +0200

    Add VS project

---
+++ CsharpProjects/TestProject/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public class CaesarCipher
+{
+    private static char[] alphabet = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+    public static string Encrypt(string text, int shift)
+    {
+        var encryptedText = new StringBuilder();
+
+        for (int i = 0; i < text.Length; i++)
+        {
+            var index = alphabet.IndexOf(text[i]);
+
+            if (index != -1)
+            {
+                var encryptedIndex = (index + shift) % alphabet.Length;
+                encryptedText.Append(alphabet[encryptedIndex]);
+            }
+            else
+            {
+                encryptedText.Append(text[i]);
+            }
+        }
+
+        return encryptedText.ToString();
+    }
+
+    public static string Decrypt(string text, int shift)
+    {
+        return Encrypt(text, -shift);
+    }
+
+    public static void Main(string[] args)
+    {
+        var text = "Hello, world!";
+        var shift = 3;
+
+        var encryptedText = Encrypt(text, shift);
+        var decryptedText = Decrypt(encryptedText, shift);
+
+        Console.WriteLine("Original text:   " + text);
+        Console.WriteLine("Encrypted text: " + encryptedText);
+        Console.WriteLine("Decrypted text: " + decryptedText);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class CaesarCipher
+{
+    static string Encrypt(string plainText, int shift)
+    {
+        char[] result = plainText.ToCharArray();
+
+        for (int i = 0; i < result.Length; i++)
+        {
+            if (Char.IsLetter(result[i]))
+            {
+                char offset = Char.IsUpper(result[i]) ? 'A' : 'a';
+                result[i] = (char)((result[i] + shift - offset) % 26 + offset);
+            }
+        }
+
+        return new string(result);
+    }
+
+    static string Decrypt(string cipherText, int shift)
+    {
+        return Encrypt(cipherText, 26 - shift);
+    }
+
+    static void Main()
+    {
+        string originalText = "Hello, World!";
+        int shift = 3;
+
+        string encryptedText = Encrypt(originalText, shift);
+        string decryptedText = Decrypt(encryptedText, shift);
+
+        Console.WriteLine("Original Text: " + originalText);
+        Console.WriteLine("Encrypted Text: " + encryptedText);
+        Console.WriteLine("Decrypted Text: " + decryptedText);
+    }
+}
